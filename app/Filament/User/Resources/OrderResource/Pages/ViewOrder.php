a95fcbea7975a40a0980c4504b240ced7d847a00
mhdzukri <zukri12737@gmail.com>
2025-09-27 12:10:49 +0700

    Nambah View Order di User

---
+++ app/Filament/User/Resources/OrderResource/Pages/ViewOrder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\User\Resources\OrderResource\Pages;
+
+use App\Filament\User\Resources\OrderResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewOrder extends ViewRecord
+{
+    protected static string $resource = OrderResource::class;
+}
