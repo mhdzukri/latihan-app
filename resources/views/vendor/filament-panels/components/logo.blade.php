175800714e2537f1dc498da7d14ef8722acc7fe8
mhdzukri <zukri12737@gmail.com>
2025-09-30 15:45:27 +0700

    change logo

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img src="{{ asset('assets/img/ulu-kasok.png') }}" alt="logo" class="h-10">
+    <img src="{{ asset('assets/img/logo-navbar.png') }}" alt="logo" class="h-10">
 </a>
